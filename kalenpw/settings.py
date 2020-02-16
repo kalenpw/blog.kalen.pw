4645f25de1bf3d10c2e587b0a150a548b4668b20
kalenpw <kalenpwilliams@gmail.com>
2020-02-16 00:09:56 -0700

    Pillow added to requirements.txt and media root corrected

--- kalenpw/settings.py
+++ kalenpw/settings.py
@@ -142,7 +142,7 @@ LOGIN_URL = 'login'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 MEDIA_URL = "media/"
-MEDIA_ROOT = "/home/kalenpw/Documents/Web/Django/Kalenpw_Blog/media"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # custom defined settings
 APP_NAME = 'Kalenpw Chatter'
