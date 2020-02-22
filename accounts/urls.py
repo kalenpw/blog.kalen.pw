db1541d7745159fcfaad1ca8a82abd137bd86b4d
kalenpw <kalenpwilliams@gmail.com>
2020-02-21 21:13:16 -0700

    Reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 
 from . import views
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login', LoginView.as_view(
         template_name='accounts/login.html'), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
+    path('password', views.change_password, name='change_password')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from django.contrib.auth import login
+from django.contrib import messages
+from django.contrib.auth import login, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import render, redirect
 
 from .forms import SignUpForm
+
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('blog:home')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'accounts/change_password.html', {
+        'form': form
+    })
 
 def signup(request):
     if request.method == 'POST':
