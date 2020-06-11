0362a1e1a14d1a0a1304438b49ff21297b5fff97
kalenpw <kalenpw@kalenpw.com>
2020-06-10 22:53:56 -0600

    No search results handled

--- blog/views/list_result.py
+++ blog/views/list_result.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from ..models import Post, Tag
 
+
 def list_result(request, tag_name):
     posts = Post.objects.filter(tags__name=tag_name)
     return render(request, 'blog/list_result.html', {'posts': posts, 'tag_name': tag_name})
