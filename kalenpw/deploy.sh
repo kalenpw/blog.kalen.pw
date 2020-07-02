4242df29b0d2404129d86631bc21738907905315
kalenpw <kalenpw@kalenpw.com>
2020-07-01 22:29:23 -0600

    Deply script and settings

---
+++ kalenpw/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+serverDir="/web/blog.kalen.pw"
+
+cd ~/blog.kalen.pw
+git pull origin master
+
+rsync -r ~/blog.kalen.pw/ "$serverDir" --delete
+
+cd $serverDir
+
+rm "$serverDir/kalenpw/settings.py"
+mv "$serverDir/kalenpw/settings_prod.py" "$serverDir/kalenpw/settings.py"
+
+rm -rf "$serverDir/venv"
+virtualenv venv -p python3
+source venv/bin/activate
+pip install -r requirements.txt
+
+./manage.py collectstatic
+
+gunicorn --daemon --workers 3 --bind unix:/web/blog.kalen.pw/kalenpw.sock kalenpw.wsgi
