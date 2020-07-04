6ef21d71364c7b43ae97cf72e5cc29ad9c83a8dd
kalenpw <kalenpw@kalenpw.com>
2020-07-03 18:13:50 -0600

    Animation on profile dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-// Generated 2020-07-03 14:41:52
+// Generated 2020-07-03 18:13:05
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
         typeof define === 'function' && define.amd ? define(factory) :
