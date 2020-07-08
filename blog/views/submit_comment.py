e7f48b58853907d4a4e112072bac8ca45d0cb667
kalenpw <kalenpw@kalenpw.com>
2020-07-07 20:50:15 -0600

    Fixes #13 - Comments can now actually be created

--- blog/views/submit_comment.py
+++ blog/views/submit_comment.py
@@ -1,9 +1,16 @@
 from django.shortcuts import redirect
 from django.urls import reverse
 
+from ..models import Comment, Post
+
 
 def submit_comment(request, post_id, post_name):
     if request.POST:
+        text = request.POST['text']
+        post = Post.objects.get(id=post_id)
+        comment = Comment(text=text, user=request.user, post=post)
+        comment.save()
+
         return redirect(reverse('blog:post_detail', args=(post_id, post_name)))
     else:
         return redirect(reverse('blog:home'))
