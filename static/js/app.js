63d547981662d15cf5de4b2e3f332cd36d12dc6a
kalenpw <kalenpw@kalenpw.com>
2020-07-07 20:14:06 -0600

    Don't show code block scroll bar when not needed

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-// Generated 2020-07-05 21:38:20
+// Generated 2020-07-07 20:12:12
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
         typeof define === 'function' && define.amd ? define(factory) :
