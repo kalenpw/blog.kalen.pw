903ec5b86b5af4885ff7c02e4a76c8ebb0255654
kalenpw <kalenpw@kalenpw.com>
2021-01-04 20:31:14 -0700

    Only enforce min lenghth on password

--- kalenpw/settings.py
+++ kalenpw/settings.py
@@ -120,27 +120,19 @@ DATABASES = {
 
 
 # Password validation
+# only enforce min length
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'OPTIONS': {
+            'min_length': 12,
+        }
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'MST'
