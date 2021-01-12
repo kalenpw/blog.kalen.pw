7b96d833900b014d9e93ae71929d92ec63c33f34
kalenpw <kalenpw@kalenpw.com>
2021-01-11 21:19:05 -0700

    Switch to sass

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-// Generated 2021-01-04 20:08:14
+// Generated 2021-01-11 20:45:25
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
         typeof define === 'function' && define.amd ? define(factory) :
