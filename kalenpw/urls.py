e8e573eae57ed48420beae39ae9d04e741ea9f0c
Kalen Williams <kwilliams@apgwest.com>
2021-06-21 16:13:03 -0600

    Add robots.txt

--- kalenpw/urls.py
+++ kalenpw/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 from rss.views import generate_rss
 
@@ -30,6 +31,7 @@ admin.site.site_title = "blog.kalen.pw"
 
 
 urlpatterns = [
+    path('robots.txt', TemplateView.as_view(template_name="core/robots.txt", content_type="text/plain")),
     path('', include('blog.urls')),
     path('', views.index, name='index'),
     path('rss/', generate_rss, name='rss'),
